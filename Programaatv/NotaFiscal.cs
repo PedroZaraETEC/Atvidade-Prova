fbf8ec7b98776fa90bc55db54c3da093d50d0689
Pedro Zara <pedro.zara@etec.sp.gov.br>
2025-04-06 14:34:04 -0300

    Atvidade completa e funcionando

--- Programaatv/NotaFiscal.cs
+++ Programaatv/NotaFiscal.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Programaatv
@@ -10,18 +11,30 @@ namespace Programaatv
     {
         public double numero;
         public Produto produto;
+        public Cliente cliente = new Cliente();
+        public Fornecedor fornecedor = new Fornecedor();
 
-        public void criaNotaFiscal(Cliente cliente, Fornecedor fornecedor)
+        public void criaNotaFiscal()
         {
+            Console.Write("Digite seu CPF: ");
+            string cpf_cliente = Console.ReadLine();
+            Console.Write("Digite seu CNPJ: ");
+            string cnpj_fornecedor = Console.ReadLine();
+
+            cliente.cpf = cpf_cliente;
+            fornecedor.cnpj = cnpj_fornecedor;
+
             if (cliente.cpf != null && fornecedor.cnpj != null)
             {
-                Console.WriteLine($"Nota num: {numero}\nCPF cliente: {cliente.cpf}\nCNPJ fornecedor: {fornecedor.cnpj}\n\n");
+                Console.WriteLine($"\n\nCPF cliente: {cliente.cpf}\nCNPJ fornecedor: {fornecedor.cnpj}\n\n");
                 Console.WriteLine("Nota criada");
+                Thread.Sleep(2000);
             } 
 
             else
             {
                 Console.WriteLine("Cliente ou fornecedor não foram encontrados");
+                Thread.Sleep(2000);
             }
 
         }
@@ -37,5 +50,6 @@ namespace Programaatv
 
             Console.WriteLine("Produto adicionado");
         }
+
     }
 }
--- Programaatv/Program.cs
+++ Programaatv/Program.cs
@@ -9,44 +9,46 @@ namespace Programaatv
 {
     internal class Program
     {
+        private static object num2;
+
         static void Main(string[] args)
         {
             string escolha;
-            char voltar = 'i';
+            string voltar = "i";
 
-            while (voltar != 'Q')
+            while (voltar != "4")
             {
-            Console.WriteLine("\t\t----Menu----\t\t\n\nBem vindo ao menu principal, aqui você conseguirá" +
+                Console.WriteLine("\t\t----Menu----\t\t\n\nBem vindo ao menu principal, aqui você conseguirá" +
                 "\ncriar um novo cliente, fornecedor, produto ou uma\nnota fiscal\n\n" +
                 "Digite abaixo oque deseja fazer: \n\n" +
                 "1 - Criar um novo fornecedor\n" +
                 "2 - Criar um novo cliente\n" +
-                "3 - Criar uma nota fiscal\n " +
-                "4 - Tudo de uma vez\n" +
-                "5 - Sair\n\n");
+                "3 - Criar uma nota fiscal\n" +
+                "4 - Sair\n\n");
 
                 Console.Write("Escolha: ");
                 escolha = Console.ReadLine();
+                voltar = escolha;
 
                 switch (escolha)
                 {
-                    case "1": 
+                    case "1":
                         Fornecedor fornecedor = new Fornecedor();
 
                         Console.Write("Digite seu nome: ");
                         string nome_fornecedor = Console.ReadLine();
                         Console.Write("Digite seu CNPJ: ");
                         string cnpj_fornecedor = Console.ReadLine();
-                        
+
                         fornecedor.criaFornecedor(nome_fornecedor, cnpj_fornecedor);
-                    break;
+                        break;
 
                     case "2":
                         Cliente cliente = new Cliente();
 
                         Console.Write("Digite seu nome: ");
                         string nome_cliente = Console.ReadLine();
-                        Console.Write("Digite seu CNPJ: ");
+                        Console.Write("Digite seu CPF: ");
                         string cpf_cliente = Console.ReadLine();
 
                         cliente.criaCliente(nome_cliente, cpf_cliente);
@@ -54,15 +56,17 @@ namespace Programaatv
 
                     case "3":
                         NotaFiscal nota_fiscal = new NotaFiscal();
+                        nota_fiscal.criaNotaFiscal();
+                    break;   
 
+                    default:
+                        Console.WriteLine("Número inesperado, Erro");
+                    break;
 
-                        nota_fiscal.criaNotaFiscal(Cliente cliente.cpf,Fornecedor fornecedor.cnpj);
-                        
-                        
-                        break;
                 }
+                    Console.Clear();
             }
-
         }
     }
 }
+
